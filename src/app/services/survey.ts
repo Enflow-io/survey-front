4e9f340fb0fda1ab2d0fe2d7eef3de56e5ce6396
Konstantin <hi@enflow.io>
2022-12-12 15:05:25 +0300

    init

---
+++ src/app/services/survey.ts
@@ -0,0 +1,87 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { buildQueries } from '@testing-library/react'
+
+export interface Survey {
+  name: string
+  id: number
+  config: string
+  link: string
+}
+
+export const surveyApi = createApi({
+  reducerPath: 'surveyApi',
+  tagTypes: ['Surveys'],
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3010/survey' }),
+  endpoints: (builder) => ({
+    getSurveyById: builder.query<Survey, string>({
+      query: (id) => `/${id}`,
+      providesTags: ['Surveys'],
+    }),
+    getSurveyByLink: builder.query<Survey, string>({
+      query: (id) => `/link/${id}`,
+      providesTags: ['Surveys'],
+    }),
+    getSurveys: builder.query({
+      query: (id) => `/`,
+      providesTags: ['Surveys'],
+    }),
+
+    addNewSurvey: builder.mutation({
+      query: (payload) => ({
+        url: '/',
+        method: 'POST',
+        body: payload,
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      }),
+      invalidatesTags: ['Surveys'],
+    }),
+    updateSurvey: builder.mutation({
+      query: ({id, ...payload}) => ({
+        url: `/${id}`,
+        method: 'PATCH',
+        body: payload,
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      }),
+      invalidatesTags: ['Surveys'],
+    }),
+    publicateSurvey: builder.mutation({
+      query: (id) => ({
+        url: `/${id}/public-id`,
+        method: 'GET',
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      }),
+      invalidatesTags: ['Surveys'],
+    }),
+    bulkDeleteSurveys: builder.mutation({
+      query: (payload) => ({
+        url: '/delete',
+        method: 'DELETE',
+        body: payload,
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      }),
+      invalidatesTags: ['Surveys'],
+    }),
+    getAnalytics: builder.query({
+      query: (id) => `/analytics`,
+    }),
+
+  }),
+})
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetSurveyByIdQuery,
+  useGetSurveysQuery, useAddNewSurveyMutation,
+  useBulkDeleteSurveysMutation,
+  useUpdateSurveyMutation,
+  usePublicateSurveyMutation,
+  useGetSurveyByLinkQuery
+} = surveyApi
