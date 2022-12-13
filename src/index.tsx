aa45f2a2eb8196685ce310f52601e57a714ce58f
Konstantin <hi@enflow.io>
2022-12-13 16:48:44 +0300

    preloaders

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useState } from 'react';
+import React, { ReactNode, useState, lazy, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
@@ -12,9 +12,9 @@ import {
 } from "react-router-dom";
 import Layout from './Layout/Layout';
 import MainPage from './Pages/MainPage';
-import SurveyPage from './Pages/SurveyPage';
+// import SurveyPage from './Pages/SurveyPage';
 import PublicSurveyPage from './Pages/PublicSurveyPage';
-import AnalyticsPage from './Pages/AnalyticsPage';
+// import AnalyticsPage from './Pages/AnalyticsPage';
 import DashboardPage from './Pages/DashboardPage';
 
 
@@ -22,6 +22,8 @@ import DashboardPage from './Pages/DashboardPage';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
+const AnalyticsPage = lazy(() => import('./Pages/AnalyticsPage'))
+const SurveyPage = lazy(() => import('./Pages/SurveyPage'))
 export const routes = [
   {
     path: "/",
@@ -33,11 +35,11 @@ export const routes = [
       },
       {
         path: "/survey/:id",
-        element: <SurveyPage />
+        element: <Suspense fallback={<div className='loader'></div>}><SurveyPage /></Suspense>
       },
       {
         path: "/survey/:id/analytics",
-        element: <AnalyticsPage />
+        element: <Suspense fallback={<div className='loader'></div>}><AnalyticsPage /></Suspense>
       },
       {
         path: "/dashboard",
@@ -45,12 +47,12 @@ export const routes = [
       },
     ]
   },
-  
+
   {
     path: "/s/:id",
     element: <PublicSurveyPage />
   },
-  
+
 ]
 const router = createBrowserRouter(routes);
 
@@ -58,9 +60,11 @@ const router = createBrowserRouter(routes);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+
       <RouterProvider router={router} />
+
     </Provider>
-  </React.StrictMode> 
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
